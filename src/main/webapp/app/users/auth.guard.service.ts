e519de369f46799154ef6f40733141be6e240600
Giacomo Angerillo <giacomo.angerillo@awisto.de>
2018-12-02 20:47:08 +0100

    Pasrt 1 finished

--- src/main/webapp/app/users/auth.guard.service.ts
+++ src/main/webapp/app/users/auth.guard.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Principal } from 'app/core';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private principal: Principal) {}
+    constructor(private principal: Principal, private router: Router) {}
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-        return this.principal.isAuthenticated();
+        const isAuthenticated = this.principal.isAuthenticated();
+        if (!isAuthenticated) {
+            this.router.navigate(['']);
+        }
+
+        return isAuthenticated;
     }
 }
--- src/main/webapp/app/users/users.module.ts
+++ src/main/webapp/app/users/users.module.ts
@@ -8,10 +8,20 @@ import { UsersService } from './users.service';
 import { AuthGuard } from './auth.guard.service';
 import { UserComponent } from './user/user.component';
 import { AdminResolver } from './admin.resolver.service';
+import { JhiLanguageService } from 'ng-jhipster';
+import { JhiLanguageHelper } from 'app/core';
 
 @NgModule({
     imports: [CommonModule, EmployeeExerciseSharedModule, RouterModule.forChild([USERS_ROUTE])],
     declarations: [UsersComponent, UserComponent],
     providers: [UsersService, AuthGuard, AdminResolver]
 })
-export class EmployeeExerciseUsersModule {}
+export class EmployeeExerciseUsersModule {
+    constructor(private languageService: JhiLanguageService, private languageHelper: JhiLanguageHelper) {
+        this.languageHelper.language.subscribe((languageKey: string) => {
+            if (languageKey !== undefined) {
+                this.languageService.changeLanguage(languageKey);
+            }
+        });
+    }
+}
